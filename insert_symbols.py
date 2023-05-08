33df0cf045b36b0bc679917823bdb9ce521414aa
Jay M Gonzalez <jaimungon@gmail.com>
2023-05-08 17:03:17 +0200

    add db connection to insert symbols

--- insert_symbols.py
+++ insert_symbols.py
@@ -1,7 +1,5 @@
 import datetime
-from math import ceil
 import sqlite3
-import requests
 from openbb_terminal.sdk import openbb
 import pandas as pd
 
@@ -12,8 +10,6 @@ def top_100_crypto():
     using requests and openbb sdk.
     Returns a pandas df to add to SQLite.
     """
-    # Stores the current time, for the created_at record
-    now = datetime.datetime.utcnow()
 
     # Get the list of the 100 cryptos by market cap
     symbolslist = openbb.crypto.ov.coin_list()[:100]
@@ -27,26 +23,27 @@ def insert_symbols(symbols):
     """
     Insert the crypto symbols into the SQLite database.
     """
+
+    # Stores the current time, for the created_at record
+    now = datetime.datetime.utcnow()
+
     # Connect to the SQLite instance
-    con = sqlite3.connect("tick_btcusdt.db")
-    cur = con.cursor()
-    res = cur.execute("SELECT name FROM sqlite_master")
-    # Create the insert strings
-    column_str = (
-        "name, instrument, ticker, sector, " "currency, created_date, last_updated_date"
-    )
-    insert_str = ("%s, " * 7)[:-2]
-    final_str = "INSERT INTO symbol (%s) VALUES (%s)" % (column_str, insert_str)
-    # Using the MySQL connection, carry out
-    # an INSERT INTO for every symbol
-    cur = con.cursor()
-    cur.executemany(final_str, symbols)
+    con = sqlite3.connect("top_100_crypto.db")
+
+    # Loop through the DataFrame and insert each row into the symbol table
+    for i, row in symbols.iterrows():
+        name = row["name"]
+        ticker = row["symbol"]
+        con.execute(
+            "INSERT INTO symbol (name, ticker, sector, currency, instrument,created_date, last_updated_date) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (name, ticker, "crypto", "USD", "spot", now, now),
+        )
+
     con.commit()
+    con.close()
 
 
 if __name__ == "__main__":
-    # print(openbb.crypto.ov.coin_list()[:100])
     symbols = top_100_crypto()
-    print(symbols)
-    # insert_snp500_symbols(symbols)
-    # print("%s symbols were successfully added." % len(symbols))
+    insert_symbols(symbols)
+    print("%s symbols were successfully added." % len(symbols))
