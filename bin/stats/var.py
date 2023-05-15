cd08f49d302bb7993e9cf4fc7220fe6fa57ab8fb
Jay M Gonzalez <jaimungon@gmail.com>
2023-05-15 12:08:44 +0200

    add var

---
+++ bin/stats/var.py
@@ -0,0 +1,12 @@
+from scipy.stats import norm
+
+
+def var_cov_var(P, c, mu, sigma):
+    """
+    Variance-Covariance calculation of daily Value-at-Risk
+    using confidence level c, with mean of returns mu
+    and standard deviation of returns sigma, on a portfolio
+    of value P.
+    """
+    alpha = norm.ppf(1 - c, mu, sigma)
+    return P - P * (alpha + 1)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bin.stats.sharpe import equity_sharpe
 from bin.sql.query import get_daily_data
+from bin.stats.var import var_cov_var
 from datetime import datetime as dt
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -8,5 +10,16 @@ if __name__ == "__main__":
     end_date = dt(2023, 1, 1)
 
     eth = get_daily_data("eth", start_date, end_date, ["adj_close_price"])
+    # SHARPE RATIO
+    # sr = equity_sharpe(eth)
+    # print(sr)
 
-    print(eth)
+    # VALUE AT RISK
+    P = 1e6  # 1,000,000 USD
+    c = 0.99  # 99% confidence interval
+    eth["rets"] = eth["adj_close_price"].pct_change()
+
+    mu = np.mean(eth["rets"])
+    sigma = np.std(eth["rets"])
+    var = var_cov_var(P, c, mu, sigma)
+    print("Value-at-Risk: $%0.2f" % var)
